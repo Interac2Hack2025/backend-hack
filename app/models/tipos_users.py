613955506da8b08006128579c83af918781573cf
Dario Cabezas <dariosce99@gmail.com>
2025-03-08 13:06:41 -0500

    Endpoints Users

--- app/models/tipos_users.py
+++ app/models/tipos_users.py
@@ -1,7 +1,7 @@
 from sqlmodel import Field, SQLModel
 from typing import Optional
 
-class tipoUsuario(SQLModel, table=True):
+class TipoUsuario(SQLModel, table=True):
     tipoId: Optional[int] = Field(default=None, primary_key=True)
     descripcion: str
     abbrev: str
--- app/models/users.py
+++ app/models/users.py
@@ -1,9 +1,9 @@
 from sqlmodel import Relationship, SQLModel, Field
 from typing import Optional
-from models.tipos_users import tipoUsuario
+from models.tipos_users import TipoUsuario
 
 class User(SQLModel, table=True):
     id: str = Field(primary_key=True)
     email: str
     tipo_user_id: Optional[int] = Field(default=None, foreign_key="tipousuario.tipoId")
-    tipoUser: Optional[tipoUsuario] = Relationship(back_populates="users")
+    tipoUser: Optional[TipoUsuario] = Relationship()
