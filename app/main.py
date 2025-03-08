034457d43a52be222eb741f1af20e95139ef2465
Dario Cabezas <dariosce99@gmail.com>
2025-03-08 12:41:41 -0500

    Creacion Base de datos

--- app/main.py
+++ app/main.py
@@ -1,13 +1,3 @@
-<<<<<<< Updated upstream
-from fastapi import FastAPI
-
-app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-=======
 from fastapi import FastAPI
 from routes import users
 from db import create_all_tables
@@ -15,4 +5,3 @@ from db import create_all_tables
 app = FastAPI(lifespan=create_all_tables)
 
 app.include_router(users.router)
->>>>>>> Stashed changes
