8bccb0f0964d33b609d4074451186d73d0498ee4
Dario Cabezas <dariosce99@gmail.com>
2025-03-08 12:40:58 -0500

    Endpoint Solucionado

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +7,12 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+=======
+from fastapi import FastAPI
+from routes import users
+from db import create_all_tables
+
+app = FastAPI(lifespan=create_all_tables)
+
+app.include_router(users.router)
+>>>>>>> Stashed changes
