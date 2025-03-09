87d969559a4d991879c8bd94a256b55d1edbed3c
Dario Cabezas <dariosce99@gmail.com>
2025-03-09 11:22:44 -0500

    CORS

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes import users, transactions
 from db import create_all_tables
 
 app = FastAPI(lifespan=create_all_tables)
+
+# Agregar middleware CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permitir orígenes de todos los dominios
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos los métodos HTTP
+    allow_headers=["*"],  # Permitir todos los encabezados
+)
 
 app.include_router(users.router)
 app.include_router(transactions.router)
